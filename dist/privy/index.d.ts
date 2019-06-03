469411c3a9a643b14f3296c0c7ba18148338afb5
Steve Thompson <writetome51@gmail.com>
2019-06-02 20:25:32 -0600

    minor change

--- dist/privy/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { getDateIDOptions, getTimeIDOptions } from './interfaces';
-export declare function __getDateOrTimeID(defaultOptions: getDateIDOptions | getTimeIDOptions, userProvidedOptions: getDateIDOptions | getTimeIDOptions, getParts: (combinedOptions: any) => Object): string;
-export declare function getTimezoneOffset(): string;
-export declare const __default_ymdOrder = "ymd";
-export declare const __default_hmsOrder = "hms";
