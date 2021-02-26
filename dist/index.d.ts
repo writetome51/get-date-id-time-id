411e8015cfdc2a34bacdf7ff688602992e333866
Steve Thompson <writetome51@gmail.com>
2021-02-25 17:26:16 -0700

    exporting more types

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+import { DateIDOptions, TimeIDOptions } from './privy/interfaces';
+
+
+export { DateIDOptions, TimeIDOptions } from './privy/interfaces';
+
+
+export declare function getDateID(options?: DateIDOptions): string;
+
+
+export declare function getTimeID(options?: TimeIDOptions): string;
+
+
+export declare function getDefaultTimeIDOptions(): TimeIDOptions;
+
+
+export declare function getDefaultDateIDOptions(): DateIDOptions;
